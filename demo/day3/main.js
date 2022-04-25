a90c0b8b4e30198752d05d5dc6b55ba594830cc6
korodoff <rudoffsutnga368@gmail.com>
2022-04-25 19:37:18 +0530

    introduction

--- demo/day3/main.js
+++ demo/day3/main.js
@@ -1,47 +1,17 @@
-// variable: boolean strings number
-// rule to assign variable
-// 1: keyword variablename=value(value are boolean strings etc)
-// 2: keyword should mainly be declarative and use camel case
-// 3: keyword are let,var,const
-// 4: names are case sensitive
-
-// let:can be declared in a line and the initialized with a value in other line
-// also declared and initialized in same line but cannot be redeclared
-// variable cannot have similar name
-
-let fName = "krut"; //declared and initialize in same line
-let sName; //declared but initialized in a different line
-sName = "krut";
-console.log(sName);
-
-// const: declared and initialized in same line only
-// cannot be redeclared and also cannot be reinitialized
-// variable cannot have similar name
-const lName = "Gupta";
-
-// let:can be declared in a line and the initialized with a value in other line
-// also declared and initialized in same line and can be redeclared
-// variable cannot have similar name
-var tName= "lana";
-var tname = "baby";
-var tName;
-tName = "honey";
-tName = "bunny";
-console.log(tname);
 // console.log(dsg); here declare a Error value is not define
 // let myHobbies; if value is not declared it will throw an error as undefine
 // console.log(myHobbies);
 // null: its a default value assign by a user to a variable as long as the variable has not ben assignned its original/update value
-let iName = null;
-console.log(iName); // o/p null
+let sName = null;
+console.log(sName); // o/p null
 // Global and Local Scope
-let rName = "krut";
-const kName = "Rose";
+let fName = "krut";
+const lName = "Rose";
 var age = 29;
 // the above value are declared as global variable
-console.log(rName, kName, age);
+console.log(fName, lName, age);
 {
-    console.log(rName, kName, age);
+    console.log(fName, lName, age);
 }
 // block scope
 {
