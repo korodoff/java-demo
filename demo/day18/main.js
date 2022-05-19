a7784c9954ae88be3893f6a9850fd2e017c3be0c
korodoff <rudoffsutnga368@gmail.com>
2022-05-19 20:16:31 +0530

    jquery

---
+++ demo/day18/main.js
@@ -0,0 +1,15 @@
+console.log("jquery + AJAX")
+
+// JS library which provide effective methid for doing somthing in frontend and essential feature
+// like DOM manipulation,Ajax interaction and so on
+
+$("#cls").click(function(){
+    // $("p").hide();
+    $("#learn").toggle();
+});
+
+$("#hide").click(function(){
+    // $("p").hide();
+    $(".learn1").toggle();
+});
+
